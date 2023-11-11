f669e4d1c7d8a7a64a454a747096d0f98b6d5eb4
Bardia Parmoun <bardiaparmoun@gmail.com>
2023-11-11 14:21:43 -0500

    Added a check for graphviz to the script.

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -4,6 +4,13 @@ input_directory="./diagrams/plantUmlFiles"
 
 plantuml="./tools/plantuml.jar"
 
+if [ $(which dot 2>/dev/null) ]; then
+  echo "Found GraphViz"
+else
+  echo "Did not find GraphViz"
+  sudo apt install graphviz
+fi
+
 for file in "$input_directory"/*.plantuml; do
     if [ -f "$file" ]; then
         java -jar "$plantuml" "$file" -o "../"
