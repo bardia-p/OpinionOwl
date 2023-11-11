19c3765040c74d7504331d7b7d3fda8e7ad75ad1
Max Curkovic <deekey45@gmail.com>
2023-11-11 10:51:17 -0500

    POST tests for API Controller

---
+++ src/test/java/com/opinionowl/opinionowl/APIControllerTest.java
@@ -0,0 +1,45 @@
+package com.opinionowl.opinionowl;
+import com.opinionowl.opinionowl.models.Survey;
+import com.opinionowl.opinionowl.repos.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Test class for the post mappings in API Controller.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class APIControllerTest {
+    @Autowired
+    private MockMvc testController;
+
+
+    @Autowired
+    private SurveyRepository surveyRepository;
+
+    /**
+     * Method to test the Create Survey post mapping. It simply verifies that a new survey was posted to the repository.
+     * @throws Exception
+     */
+    @Test
+    public void testCreateSurvey() throws Exception{
+        String postData = "{\"radioQuestions\":{\"Test2\":[\"a\",\"b\"]},\"numericRanges\":{\"Test3\":[0,11]},\"title\":\"This is a test\",\"textQuestions\":[\"Test1\"]}";
+
+        this.testController.perform(post("/api/v1/createSurvey")
+                        .contentType(MediaType.APPLICATION_JSON).content(postData))
+                .andExpect(status().isOk());
+
+        Survey survey = surveyRepository.findById(1L);
+        System.out.println(survey);
+    }
+}
+
