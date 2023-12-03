b88e1b333c77a4825e274981f8d08bd0098010c7
Max Curkovic <91294718+maxcurkovic@users.noreply.github.com>
2023-12-03 17:37:26 -0500

    Resolve Issue 72 User Input Validation (#85)
    Added failsafes for user registration

--- src/main/java/com/opinionowl/opinionowl/controllers/APIController.java
+++ src/main/java/com/opinionowl/opinionowl/controllers/APIController.java
@@ -290,6 +290,11 @@ public class APIController {
         HashMap<String, String> userData = objectMapper.readValue(jsonData, new TypeReference<HashMap<String, String>>() {});
         String username = userData.get("username");
         String password = userData.get("password");
+        for (AppUser appUser: userRepo.findAll()){
+            if (appUser.getUsername().equals(username)){
+                return 401;
+            }
+        }
         AppUser appUser = new AppUser(username, password);
         userRepo.save(appUser);
         System.out.println(appUser);
