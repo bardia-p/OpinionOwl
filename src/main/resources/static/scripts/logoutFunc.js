d8aa5240a58f7d406892ca4989765c81e39b6be6
Anthony Massaad <massaadanthony@hotmail.com>
2023-12-04 13:51:36 -0500

    updated header

--- src/main/resources/static/scripts/logoutFunc.js
+++ src/main/resources/static/scripts/logoutFunc.js
@@ -1,4 +1,16 @@
 const logoutButton = $("#logout-btn");
+const profileName = $("#profile-name");
+const dropDownSelections = $("#drop-down-selections");
+
+profileName.click(() => {
+    if (dropDownSelections.css("opacity") === "1") {
+        dropDownSelections.css("opacity", "0");
+        dropDownSelections.css("visibility", "hidden");
+        return;
+    }
+    dropDownSelections.css("opacity", "1");
+    dropDownSelections.css("visibility", "visible");
+})
 
 /**
  * The JavaScript AJAX call for when a user logs out of their account
