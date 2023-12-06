f70aabcd631f69298c2856c1dd9cae31b511ce5c
Max Curkovic <deekey45@gmail.com>
2023-12-05 20:08:43 -0500

    Initial commit - set up pom.xml and main

---
+++ src/main/java/com/opinionowl/opinionowl/LoginAspect.java
@@ -0,0 +1,13 @@
+package com.opinionowl.opinionowl;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+
+}
--- src/main/java/com/opinionowl/opinionowl/OpinionOwlApplication.java
+++ src/main/java/com/opinionowl/opinionowl/OpinionOwlApplication.java
@@ -2,11 +2,13 @@ package com.opinionowl.opinionowl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * The starting point for the application.
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class OpinionOwlApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpinionOwlApplication.class, args);
